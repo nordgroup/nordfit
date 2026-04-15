21e2c5a4136100503d221b9530e84994156163cd
louis <nordgroup.business@gmail.com>
2026-04-15 18:00:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-// 🖱️ Mausbewegung erzeugt leichte Bewegung der Karten (Apple Feeling)
+// 🌫 leichte Mausbewegung für „Liquid Feeling“
 document.addEventListener("mousemove", (e) => {
 
-  // X Position der Maus (zentriert berechnet)
-  const x = (e.clientX / window.innerWidth - 0.5) * 10;
+  const x = (e.clientX / window.innerWidth - 0.5) * 4;
+  const y = (e.clientY / window.innerHeight - 0.5) * 4;
 
-  // Y Position der Maus
-  const y = (e.clientY / window.innerHeight - 0.5) * 10;
-
-  // Alle Glass-Karten bewegen sich leicht mit der Maus
   document.querySelectorAll(".glass").forEach(el => {
     el.style.transform = `translate(${x}px, ${y}px)`;
   });
+
 });
